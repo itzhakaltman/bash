2e1060a69649e37822445d4a6b0e6df0d1af7da1
Itzhak Altman <96288626+itzhakaltman@users.noreply.github.com>
2022-03-27 07:11:00 +0300

    270301

---
+++ I/4 - Shell Scripting/if.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+
+echo "Hello!"
+
+if [ 3 -eq 5 ]; then
+	echo "Yes, they are the same!"
+fi
+
+echo "Continue with our program.."
+
+echo "bye:)"
+
+
+# 3 -eq 3      3 = 3
+# 3 -ne 4      3 is not 4
+# 3 -gt 1      3 > 1
+# 3 -lt 7      3 < 7
+
+# 3 -ge 3      3 >= 3
+# 3 -le 3      3 <= 3
